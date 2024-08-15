02dffbd0057a29619e5151d3b6080ecaa6daf5b6
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2024-08-14 22:53:15 -0500

    us-east-1 added and fixed

--- examples/karpenter/main.tf
+++ examples/karpenter/main.tf
@@ -42,7 +42,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 
 locals {
   name   = "ex-${basename(path.cwd)}"
-  region = "eu-west-1"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
